ee8f43ba3ba13222c3d1045262d48aeb72eb9c97
Lydia <lreitzel@gmail.com>
2021-02-22 23:05:02 -0500

    Fourth commit

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,15 @@
 class Activity
+    attr_accessor :name
+
+    @@all =[]
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
 end
--- lib/park.rb
+++ lib/park.rb
@@ -1,2 +1,17 @@
 class Park
+    attr_accessor :name, :description, :location, :activity
+
+    @@all = []
+
+    def initialize(name)
+        @name = fullName
+        @description = description
+        @location = addresses
+        @activity = activities
+    end
+
+    def self.all
+        @@all 
+    end
+
 end
